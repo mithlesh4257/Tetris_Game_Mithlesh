d6d0c71c38fd651bc9ab2648807226da815bb1f5
mithlesh4257 <mithlesh4257@iitg.ernet.in>
2017-06-29 08:57:29 -0700

    Update tetris.js

--- tetris.js
+++ tetris.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
 context.fillStyle = '#000';
-context.fillRect(0,0,canvas.width,canvas.height);
+tcontext.fillRect(0,0,canvas.width,canvas.height);
 
 const matrix = [
 		[0,0,0],
@@ -16,17 +16,17 @@ function drawMatrix(matrix,offset){
  		 if(value !== 0){
 			 context.fillStyle = 'red';
 			 context.fillRect(x + offset.x, y+offset.y, 1, 1);
-			}
+[			}
 		}
 	}
 }
 
 function merge(arena, player){
- player.matrix.forEach((row, y) => {
+player.matrix.forEach((row, y) => {
   row.forEach((value,x));
 });
 }
-
+]
 function playerDrop(){
  player.pos.y++;
  dropCounter =0;
@@ -36,7 +36,7 @@ let dropCounter = 0;
 let dropInterval = 1000;
 
  let lastTime = 0;
-function update(){
+[function update(){
 const deltaTime = time - lastTime;
 lastTime - time;
 console.log(deltaTime);
@@ -44,8 +44,9 @@ draw();
 requestAnimationFrame(update);
 }
 
+		
 const player = {
- pos: {x: 5, y: 5},
+] pos: {x: 5, y: 5},
  matrix: matrix
 };
 
@@ -63,18 +64,40 @@ function playerMove(dir){
 }
 
 function playerRotate(dir){
+	const pos = player.pos.x;
+	let offset = 1;
   rotate(player.matrix, dir);
  while(collide(arena,matrix)){
   player.pos.x += offset;
   offset = -(offset+(offset> 0?1:-1)); 
   if(offset > player.matrix[0].length){
    rotate(player.matrix, -dir);
-   
+   player.pos.x = pos;
    return;
 }
 }
 }
 
+function createPiece(type){
+	if(type === 'T'){
+	 return [
+		 [0,0],[1,1]
+		 ];
+	}
+	else if(type ==='O'){
+		return [
+
+                 [1,1,1],[0,0,0],[1,0,1]
+
+                 ];
+	}
+}
+		 function playerReset(){
+			const pieces = 'ILJOTSZ';	 
+			 player.matrix = createPiece(pieces[pieces.length* Math.random() | 0]);
+			 player.pos.y = 0;
+			 player.pos.x = (arena[0].length/2|0) - (player.matrix[0].length/2 |0);
+		 }
 function rotate(matrix, dir){
  for(let y=0;y< matrix.length;++y){
  for(let x =0;x<y;++x){
@@ -88,7 +111,10 @@ function rotate(matrix, dir){
 }
 }
  if(dir > 0){
-  
+  matrix.forEach(row => row.reverse());
 }
+	else {
+	   	
+	}
 
 }
